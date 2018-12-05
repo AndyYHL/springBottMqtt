0aa244481577f9758d3bb5fe09aa60950b9c70f6
yhl <1004655482@qq.com>
2018-12-05 14:43:04 +0800

    新增redis 配置

---
+++ src/main/java/com/tuyou/mqtt/producer/pojo/vo/User.java
@@ -0,0 +1,12 @@
+package com.tuyou.mqtt.producer.pojo.vo;
+
+import lombok.Data;
+
+/**
+ * @author yhl
+ */
+@Data
+public class User {
+    String userName;
+    String pwd;
+}
---
+++ src/main/java/com/tuyou/mqtt/producer/web/controller/IndexController.java
@@ -0,0 +1,37 @@
+package com.tuyou.mqtt.producer.web.controller;
+
+import com.tuyou.mqtt.producer.pojo.vo.User;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author yhl
+ */
+@RestController
+@RequestMapping(value = "/")
+public class IndexController {
+    @RequestMapping(value = "/session")
+    public String getSession(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        if (user == null){
+            return "用好未登录" + session.getId();
+        } else {
+            return "用好已登录" + session.getId();
+        }
+    }
+
+    @RequestMapping(value = "/get")
+    public String get(HttpServletRequest request) {
+        User user = new User();
+        user.setUserName("chf");
+        user.setPwd("pass");
+        request.getSession().setAttribute("user", user);
+
+        return "登录成功 "  + request.getSession().getId();
+    }
+}
