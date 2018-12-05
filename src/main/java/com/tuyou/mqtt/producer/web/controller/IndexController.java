03d2ea839494e5f4b582f5446cd3f2512466d9f2
yhl <1004655482@qq.com>
2018-12-05 18:09:14 +0800

    删除多余的引用

--- src/main/java/com/tuyou/mqtt/producer/web/controller/IndexController.java
+++ src/main/java/com/tuyou/mqtt/producer/web/controller/IndexController.java
@@ -1,7 +1,6 @@
 package com.tuyou.mqtt.producer.web.controller;
 
 import com.tuyou.mqtt.producer.pojo.vo.User;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
