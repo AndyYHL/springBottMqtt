54b16e2ae3fd10d543f2b311af4e938f84b5e9e7
yhl <1004655482@qq.com>
2018-12-11 13:27:45 +0800

    格式化代码

--- src/main/java/com/tuyou/mqtt/producer/service/impl/EquipmentDataServiceImpl.java
+++ src/main/java/com/tuyou/mqtt/producer/service/impl/EquipmentDataServiceImpl.java
@@ -14,79 +14,85 @@ import com.tuyou.mqtt.producer.pojo.vo.EquipmentDataVO;
 import org.springframework.beans.BeanUtils;
 import org.apache.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-@Service
-public class EquipmentDataService extends ServiceImpl<EquipmentDataMapper, EquipmentDataDO> implements IEquipmentDataService {
 /**
-     * @Description 新增设备数据
+ * @author yhl
+ * 采集数据操作类
+ */
+@Service
+public class EquipmentDataServiceImpl extends ServiceImpl<EquipmentDataMapper, EquipmentDataDO> implements IEquipmentDataService {
+    /**
      * @param equipmentDataDTO 请求equipmentDataDTO数据
      * @return 响应Integer数据
+     * @Description 新增设备数据
      */
     @Override
-    public Integer saveEquipmentData(EquipmentDataDTO equipmentDataDTO){
-    	return 0;
+    public Integer saveEquipmentData(EquipmentDataDTO equipmentDataDTO) {
+        return 0;
     }
 
     /**
      * 保存非空值
+     *
      * @param equipmentDataDTO
      * @return 响应Integer数据
      */
     @Override
-    public Integer saveEquipmentDataNotNull(EquipmentDataDTO equipmentDataDTO){
-    	return 0;
+    public Integer saveEquipmentDataNotNull(EquipmentDataDTO equipmentDataDTO) {
+        return 0;
     }
 
     /**
-     * @Description 修改设备数据
      * @param equipmentDataDTO 请求equipmentDataDTO数据
      * @return 响应boolean数据
+     * @Description 修改设备数据
      */
     @Override
-    public boolean uptEquipmentData(EquipmentDataDTO equipmentDataDTO){
-    	return false;
+    public boolean uptEquipmentData(EquipmentDataDTO equipmentDataDTO) {
+        return false;
     }
 
     /**
-     * @Description 删除设备数据
      * @param equipmentDataDTO 请求equipmentDataDTO数据
      * @return 响应Integer数据
+     * @Description 删除设备数据
      */
     @Override
-    public Integer delEquipmentData(EquipmentDataDTO equipmentDataDTO){
-    	return 0;
+    public Integer delEquipmentData(EquipmentDataDTO equipmentDataDTO) {
+        return 0;
     }
 
     /**
-     * @Description 查询设备数据详情
      * @param equipmentDataDTO 请求equipmentDataDTO数据
      * @return 响应EquipmentDataVO数据
+     * @Description 查询设备数据详情
      */
     @Override
-    public EquipmentDataVO findEquipmentData(EquipmentDataDTO equipmentDataDTO){
-    	return null;
+    public EquipmentDataVO findEquipmentData(EquipmentDataDTO equipmentDataDTO) {
+        return null;
     }
 
     /**
+     * @param equipmentDataDTO 请求equipmentDataDTO数据
+     * @return 响应List<equipmentDataVO>数据
      * @Description 查询列表设备数据列表 分页可有可无
-     * @param equipmentDataDTO 请求equipmentDataDTO数据
-     * @return 响应List<equipmentDataVO>数据
      */
     @Override
-    public List<EquipmentDataVO> findEquipmentDataList(EquipmentDataDTO equipmentDataDTO){
-    	return null;
+    public List<EquipmentDataVO> findEquipmentDataList(EquipmentDataDTO equipmentDataDTO) {
+        return null;
     }
 
     /**
-     * @Description 查询列表设备数据(带分页)
      * @param equipmentDataDTO 请求equipmentDataDTO数据
      * @return 响应List<equipmentDataVO>数据
+     * @Description 查询列表设备数据(带分页)
      */
     @Override
-    public List<EquipmentDataVO> findEquipmentDataLimit(EquipmentDataDTO equipmentDataDTO, ExtLimit extLimit){
-    	return null;
+    public List<EquipmentDataVO> findEquipmentDataLimit(EquipmentDataDTO equipmentDataDTO, ExtLimit extLimit) {
+        return null;
     }
 }
--- src/main/java/com/tuyou/mqtt/producer/service/impl/EquipmentInfoServiceImpl.java
+++ src/main/java/com/tuyou/mqtt/producer/service/impl/EquipmentInfoServiceImpl.java
@@ -14,79 +14,85 @@ import com.tuyou.mqtt.producer.pojo.vo.EquipmentInfoVO;
 import org.springframework.beans.BeanUtils;
 import org.apache.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-@Service
-public class EquipmentInfoService extends ServiceImpl<EquipmentInfoMapper, EquipmentInfoDO> implements IEquipmentInfoService {
 /**
-     * @Description 新增设备信息
+ * @author yhl
+ * 设备操作操作类
+ */
+@Service
+public class EquipmentInfoServiceImpl extends ServiceImpl<EquipmentInfoMapper, EquipmentInfoDO> implements IEquipmentInfoService {
+    /**
      * @param equipmentInfoDTO 请求equipmentInfoDTO数据
      * @return 响应Integer数据
+     * @Description 新增设备信息
      */
     @Override
-    public Integer saveEquipmentInfo(EquipmentInfoDTO equipmentInfoDTO){
-    	return 0;
+    public Integer saveEquipmentInfo(EquipmentInfoDTO equipmentInfoDTO) {
+        return 0;
     }
 
     /**
      * 保存非空值
+     *
      * @param equipmentInfoDTO
      * @return 响应Integer数据
      */
     @Override
-    public Integer saveEquipmentInfoNotNull(EquipmentInfoDTO equipmentInfoDTO){
-    	return 0;
+    public Integer saveEquipmentInfoNotNull(EquipmentInfoDTO equipmentInfoDTO) {
+        return 0;
     }
 
     /**
-     * @Description 修改设备信息
      * @param equipmentInfoDTO 请求equipmentInfoDTO数据
      * @return 响应boolean数据
+     * @Description 修改设备信息
      */
     @Override
-    public boolean uptEquipmentInfo(EquipmentInfoDTO equipmentInfoDTO){
-    	return false;
+    public boolean uptEquipmentInfo(EquipmentInfoDTO equipmentInfoDTO) {
+        return false;
     }
 
     /**
-     * @Description 删除设备信息
      * @param equipmentInfoDTO 请求equipmentInfoDTO数据
      * @return 响应Integer数据
+     * @Description 删除设备信息
      */
     @Override
-    public Integer delEquipmentInfo(EquipmentInfoDTO equipmentInfoDTO){
-    	return 0;
+    public Integer delEquipmentInfo(EquipmentInfoDTO equipmentInfoDTO) {
+        return 0;
     }
 
     /**
-     * @Description 查询设备信息详情
      * @param equipmentInfoDTO 请求equipmentInfoDTO数据
      * @return 响应EquipmentInfoVO数据
+     * @Description 查询设备信息详情
      */
     @Override
-    public EquipmentInfoVO findEquipmentInfo(EquipmentInfoDTO equipmentInfoDTO){
-    	return null;
+    public EquipmentInfoVO findEquipmentInfo(EquipmentInfoDTO equipmentInfoDTO) {
+        return null;
     }
 
     /**
+     * @param equipmentInfoDTO 请求equipmentInfoDTO数据
+     * @return 响应List<equipmentInfoVO>数据
      * @Description 查询列表设备信息列表 分页可有可无
-     * @param equipmentInfoDTO 请求equipmentInfoDTO数据
-     * @return 响应List<equipmentInfoVO>数据
      */
     @Override
-    public List<EquipmentInfoVO> findEquipmentInfoList(EquipmentInfoDTO equipmentInfoDTO){
-    	return null;
+    public List<EquipmentInfoVO> findEquipmentInfoList(EquipmentInfoDTO equipmentInfoDTO) {
+        return null;
     }
 
     /**
-     * @Description 查询列表设备信息(带分页)
      * @param equipmentInfoDTO 请求equipmentInfoDTO数据
      * @return 响应List<equipmentInfoVO>数据
+     * @Description 查询列表设备信息(带分页)
      */
     @Override
-    public List<EquipmentInfoVO> findEquipmentInfoLimit(EquipmentInfoDTO equipmentInfoDTO, ExtLimit extLimit){
-    	return null;
+    public List<EquipmentInfoVO> findEquipmentInfoLimit(EquipmentInfoDTO equipmentInfoDTO, ExtLimit extLimit) {
+        return null;
     }
 }
