d93c4ebab2fdffd42ea0322837418b26dc69847d
yhl <1004655482@qq.com>
2018-12-13 18:05:38 +0800

    新增获取采集数据

---
+++ src/main/java/com/tuyou/mqtt/producer/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.tuyou.mqtt.producer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 跨域处理
+ * @author yhl
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .maxAge(3600);
+    }
+}
