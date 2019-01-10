87490f75d2cb461c55fd257b2083472094b406ff
yhl <1004655482@qq.com>
2019-01-10 16:12:41 +0800

    加入加密工具类

---
+++ src/main/java/com/tuyou/mqtt/producer/util/CryptUtils.java
@@ -0,0 +1,67 @@
+package com.tuyou.mqtt.producer.util;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+
+/**
+ * MD5 加密
+ * @author
+ */
+public class CryptUtils {
+    private static final String KEY_MD5 = "MD5";
+    // 全局数组
+    private static final String[] strDigits = { "0", "1", "2", "3", "4", "5",
+            "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
+
+    // 返回形式为数字跟字符串
+    private static String byteToArrayString(byte bByte) {
+        int iRet = bByte;
+        if (iRet < 0) {
+            iRet += 256;
+        }
+        int iD1 = iRet / 16;
+        int iD2 = iRet % 16;
+        return strDigits[iD1] + strDigits[iD2];
+    }
+
+    // 转换字节数组为16进制字串
+    private static String byteToString(byte[] bByte) {
+        StringBuffer sBuffer = new StringBuffer();
+        for (int i = 0; i < bByte.length; i++) {
+            sBuffer.append(byteToArrayString(bByte[i]));
+        }
+        return sBuffer.toString();
+    }
+    /**
+     * MD5加密
+     * @param strObj
+     * @return
+     * @throws Exception
+     */
+    public static String getMD5Code(String strObj) throws Exception{
+        MessageDigest md5 = null;
+        try{
+            md5 = MessageDigest.getInstance(KEY_MD5);
+        }catch(Exception e){
+            System.out.println(e.toString());
+            e.printStackTrace();
+            return "";
+        }
+        byte[] byteArray = null;
+        try {
+            byteArray = strObj.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        byte[] md5Bytes = md5.digest(byteArray);
+        StringBuffer hexValue = new StringBuffer();
+        for(int i=0;i<md5Bytes.length;i++){
+            int val = md5Bytes[i] & 0xff;
+            if(val<16){
+                hexValue.append("0");
+            }
+            hexValue.append(Integer.toHexString(val));
+        }
+        return hexValue.toString();
+    }
+}
